6315147a49b1e68f8e495ae634d45e493d007447
thanh tung <meomeo1998x@gmail.com>
2020-04-25 20:55:01 +0700

    Migration

---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+            var ConnectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
